3681d60c6bcdf8f84b43966af654a2062a3fd791
Pedro Andrade <pedroandrade2020@gmail.com>
2016-04-01 00:07:05 +0100

    Having a bit of fun with Attribute/Traditional Routing

--- Ottav/Ottav/App_Start/RouteConfig.cs
+++ Ottav/Ottav/App_Start/RouteConfig.cs
@@ -9,14 +9,39 @@ namespace Ottav
 {
     public class RouteConfig
     {
+        // Separated Attribute Routes Register with the Traditional ones because of Unit Testing
         public static void RegisterRoutes(RouteCollection routes)
         {
+            // Enable Attribute Routing
+            //routes.MapMvcAttributeRoutes(); 
+            RegisterTraditionalRoutes(routes);
+        }
+
+        public static void RegisterTraditionalRoutes(RouteCollection routes)
+        {
+            // Routes to be ignored 
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    name: "Valor",
+            //    url: "{controller}/About/{valor}",
+            //    defaults: new { controller = "Home", action = "Index" },
+            //    constraints: new { productId = @"\d+" }
+            //);
+
+            // Default Route
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                //constraints: new { controller = "Home|Settings|General|..." }
+            );
+
+            // Couldn't find a route to handle the request.  Show the 404 page.
+            routes.MapRoute(
+                name: "NotFound",
+                url: "{*url}",
+                defaults: new { controller = "Error", action = "PageNotFound" }
             );
         }
     }
--- Ottav/Ottav/Controllers/HomeController.cs
+++ Ottav/Ottav/Controllers/HomeController.cs
@@ -6,20 +6,33 @@ using System.Web.Mvc;
 
 namespace Ottav.Controllers
 {
+    [RoutePrefix("home")]
+    [Route("{action=Index}")]
+    //[Route("home/{action}")]
     public class HomeController : Controller
     {
+        [Route("~/")]
+        [Route("~/badjoras")]           // Escape from RoutePrefix
+        [Route("")]
+        //[Route("index")]
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        //[Route("about")]
+        //[Route("about/{valor}")]
+        //public ActionResult About(int valor)
+        public ActionResult About(int valor)
+
         {
             ViewBag.Message = "Your application description page.";
+            ViewBag.valor = valor;
 
             return View();
         }
 
+        //[Route("contact")]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
