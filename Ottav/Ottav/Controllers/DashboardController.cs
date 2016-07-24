e50f8bc148b3ca5d21dc175d667712f1bb5f969b
Pedro Andrade <pedroandrade2020@gmail.com>
2016-07-25 00:30:12 +0100

    -- Changes - Can't remember

---
+++ Ottav/Ottav/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ottav.Controllers
+{
+    public class DashboardController : Controller
+    {
+        //
+        // GET: Dashboard/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: Dashboard/List
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
